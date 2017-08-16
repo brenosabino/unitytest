101c9660909e5ef206e59c70323172c3abf2cb4d
BRENO ALVES LEMOS SABINO <brenosabino@unp.edu.br>
2017-08-15 21:29:56 -0300

    mmmmmm

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Leva em consideração a fisica
+	void FixedUpdate (){
+	
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+
+        rb.AddForce(movement * speed);
+			
+	}
+}
